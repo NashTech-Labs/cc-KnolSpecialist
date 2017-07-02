39190687437ab66d9a1ddfc5793a6e218f79a47c
pranjut <pranjut.gogoi@gmail.com>
2017-07-03 01:12:34 +0530

    Added welcome page

--- ks-api/src/main/scala/com/knoldus/cc/ks/api/KnolSpecialistService.scala
+++ ks-api/src/main/scala/com/knoldus/cc/ks/api/KnolSpecialistService.scala
@@ -4,14 +4,23 @@ import akka.NotUsed
 import com.knoldus.cc.ks.Technology
 import com.lightbend.lagom.scaladsl.api.{Descriptor, Service, ServiceCall}
 
-trait KnolSpecialistService extends Service{
-import com.knoldus.cc.ks.JsonSerial._
+import scala.concurrent.Future
+
+trait KnolSpecialistService extends Service {
+
+  import com.knoldus.cc.ks.JsonSerial._
+
   def getTechnique(id: Int): ServiceCall[NotUsed, Technology]
+
+  def welcome: ServiceCall[NotUsed, String] = ServiceCall { request =>
+    Future.successful("Welcome to cc-specialist")
+  }
 
   override def descriptor: Descriptor = {
     import Service._
     named("ks")
       .withCalls(
+        pathCall("/", welcome _),
         pathCall("/api/get/technology/:id", getTechnique _)
       )
       .withAutoAcl(true)

9478b0ae4aeb9653b41eaeeb7a9090749a204289
Ashu-Tomar <ashu@knoldus.com>
2017-07-07 18:30:27 +0530

    added experts dummy service on frontend

--- frontend/src/app/experts/experts.component.ts
+++ frontend/src/app/experts/experts.component.ts
@@ -1,10 +1,15 @@
 import {Component} from '@angular/core';
 import {selector} from "rxjs/operator/publish";
+import {ExpertsService} from "./experts.service";
 @Component({
   moduleId: module.id,
   selector: 'experts',
   templateUrl : 'experts.component.html'
 })
 export class ExpertsComponent{
+  expertsArray:any[];
 
+  constructor(expertService: ExpertsService){
+    this.arr = expertService.experts();
+  }
 }
--- frontend/src/dist/app/experts/experts.component.js
+++ frontend/src/dist/app/experts/experts.component.js
@@ -5,9 +5,14 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 var core_1 = require("@angular/core");
+var experts_service_1 = require("./experts.service");
 var ExpertsComponent = (function () {
-    function ExpertsComponent() {
+    function ExpertsComponent(expertService) {
+        this.arr = expertService.experts();
     }
     return ExpertsComponent;
 }());
@@ -16,7 +21,8 @@ ExpertsComponent = __decorate([
         moduleId: module.id,
         selector: 'experts',
         templateUrl: 'experts.component.html'
-    })
+    }),
+    __metadata("design:paramtypes", [experts_service_1.ExpertsService])
 ], ExpertsComponent);
 exports.ExpertsComponent = ExpertsComponent;
 //# sourceMappingURL=experts.component.js.map
